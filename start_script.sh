0c4e7ab0174c159e52e97ed4c48b74c81b865219
Camille Viot <viot.camille@outlook.com>
2021-01-25 19:28:39 +0100

    up

---
+++ start_script.sh
@@ -0,0 +1,4 @@
+curl -Lo gleam.tar.gz https://github.com/gleam-lang/gleam/releases/download/v0.13.2/gleam-v0.13.2-linux-amd64.tar.gz
+tar xf gleam.tar.gz
+
+# https://gitpod.io/#https://github.com/codec-abc/gitpod-gleam
